9cea38569d61dcb77f61c7e9570f69e5f67b5276
willy90510 <88758212+willy90510@users.noreply.github.com>
2023-03-25 12:41:10 +0800

    3 files modified

--- src/main.ts
+++ src/main.ts
@@ -17,13 +17,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 export class App {
   name = 'Angular';
 }
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
 
-  // Otherise, log the boot error
-}).catch(err => console.error(err));
 bootstrapApplication(App);
