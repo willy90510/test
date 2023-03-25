a3e53f630d02c0c6c2b70b2e7d2ce6068e29ad75
willy90510 <88758212+willy90510@users.noreply.github.com>
2023-03-25 18:03:03 +0800

    4 files modified

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,24 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTabsModule],
   template: `
     <h1>Hello from {{name}}!</h1>
     <a target="_blank" href="https://angular.io/start">
       Learn more about Angular 
     </a>
   `,
+  templateUrl: 'index.html',
+  styleUrls: ['global_styles.css'],
 })
 export class App {
   name = 'Angular';
+  viewMode = 'map';
 }
 
 bootstrapApplication(App);
